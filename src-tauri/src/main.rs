ff1bba5df96458598f45e5ffaf4cd701b5ed88f3
durpy <89787577+durpyneko@users.noreply.github.com>
2024-06-04 12:39:36 +0200

    rocket server serves dynamic image
    Start a Rocket server to serve the image from disk so you are able to change it, since the frontend can't read from disk. Overkill? Yes. Works? Also yes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,50 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+#[macro_use]
+extern crate rocket;
+
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::fs::NamedFile;
+use rocket::http::Header;
+use rocket::Response;
+use rocket::{Request, Rocket};
 use rodio::{Decoder, Source};
+use std::path::Path;
 use std::sync::{Arc, Mutex};
 use tauri::Manager;
+
+#[get("/image")]
+async fn image() -> Option<NamedFile> {
+    NamedFile::open(Path::new("assets/images/catto.png"))
+        .await
+        .ok()
+}
+
+// probably not a great idea
+struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "GET, POST, PUT, DELETE, OPTIONS",
+        ));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Headers",
+            "Content-Type, Authorization",
+        ));
+    }
+}
 
 fn play(window: Arc<Mutex<tauri::Window>>) {
     // TODO: Read filepath/filename from config (settings)
@@ -16,6 +57,13 @@ fn play_audio(
     file_path: &str,
     window: Arc<Mutex<tauri::Window>>,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    // dev is lazy so prob a TODO here
+    // wait for the DOM to load via seconds
+    // instead of telling tauri from the frontend
+    // that it has been loaded
+    // lmao, suck my dick uwu
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
     // Create an output stream
     let (_stream, stream_handle) = rodio::OutputStream::try_default()?;
 
@@ -50,9 +98,18 @@ struct AppState {
     window: Arc<Mutex<tauri::Window>>,
 }
 
-fn main() {
+#[rocket::main]
+async fn main() {
     tauri::Builder::default()
         .setup(|app| {
+            tauri::async_runtime::spawn(async move {
+                let _rocket = rocket::build() // .configure(rocket::Config::figment().merge(("port", 133769)))
+                    .attach(CORS)
+                    .mount("/", routes![image])
+                    .launch()
+                    .await;
+            });
+
             let window = app.get_window("main").unwrap();
             window.show().unwrap();
             window.set_focus().unwrap();
